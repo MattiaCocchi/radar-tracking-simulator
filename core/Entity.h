024c756b6ca9e7ac07bd2f317491eb8dc4c6c0de
MattiaCocchi <matti19.cocchi@gmail.com>
2026-05-11 22:13:53 +0200

    Base classes created

---
+++ core/Entity.h
@@ -0,0 +1,14 @@
+#include <iostream>
+
+struct State{
+    double x, y, vx, vy;
+};
+
+class Entity{
+    private:
+    State e_state;
+    public:
+    Entity(double x, double y, double vx, double vy);
+    void update(double dt);
+    State getState();
+};
---
+++ core/Radar.h
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <random>
+
+class Radar{
+    private:
+    std::mt19937 m_gen;
+    std::normal_distribution<double> n_dist;
+    public:
+    std::pair<double, double> detect(double trueX, double trueY);
+    Radar(double sigma);
+};
